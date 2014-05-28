85617f46d5993899b4e23d97794a96d4ba8325e0
Pavel Bariev <bariew@yandex.ru>
2014-05-28 18:54:17 +0400

    added _events.php

---
+++ _events.php
@@ -0,0 +1,13 @@
+<?php  
+return [
+    'app\modules\user\models\RegisterForm' => [
+        'afterInsert' => [
+            ['bariew\noticeModule\models\Notice', 'userRegistration']
+        ],  
+    ],
+//    'app\modules\user\models\User' => [
+//        'afterUpdate' => [
+//            ['bariew\noticeModule\models\Notice', 'test']
+//        ],  
+//    ],
+];
